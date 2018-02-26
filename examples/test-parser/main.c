670861af51b15d40b585f231ca54f3570c3dfd1b
Pascal Dupuis <cdemills@gmail.com>
2018-02-26 18:15:06 +0100

    Makefile: calls gperf; main.c: adapted to the new interface

--- examples/test-parser/main.c
+++ examples/test-parser/main.c
@@ -91,12 +91,13 @@ int main(int argc, char** argv) {
     int result;
 
     SCPI_Init(&scpi_context,
-            scpi_commands,
-            &scpi_interface,
-            scpi_units_def,
-            SCPI_IDN1, SCPI_IDN2, SCPI_IDN3, SCPI_IDN4,
-            scpi_input_buffer, SCPI_INPUT_BUFFER_LENGTH,
-            scpi_error_queue_data, SCPI_ERROR_QUEUE_SIZE);
+              &in_scpicmd_name,
+              scpi_commands_var,
+              &scpi_interface,
+              scpi_units_def,
+              SCPI_IDN1, SCPI_IDN2, SCPI_IDN3, SCPI_IDN4,
+              scpi_input_buffer, SCPI_INPUT_BUFFER_LENGTH,
+              scpi_error_queue_data, SCPI_ERROR_QUEUE_SIZE);
 
 #define TEST_SCPI_INPUT(cmd)    result = SCPI_Input(&scpi_context, cmd, strlen(cmd))
 
